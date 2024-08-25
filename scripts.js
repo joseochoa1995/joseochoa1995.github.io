ce3a7b656bcab03a9bb223bc6836b72df3e3c4bc
joseochoa1995 <135753409+joseochoa1995@users.noreply.github.com>
2024-08-25 18:24:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const bannerWrapper = document.getElementById('bannerWrapper');
-    const iframes = document.querySelectorAll('.section .video-item iframe');
-    
-    bannerWrapper.innerHTML = '';
+  const bannerWrapper = document.getElementById('bannerWrapper');
+  const iframes = document.querySelectorAll('.section .video-item iframe');
   
-    iframes.forEach(iframe => {
-      const clone = iframe.cloneNode(true);
-      const bannerItem = document.createElement('div');
-      bannerItem.className = 'banner-item';
-      bannerItem.appendChild(clone);
-      bannerWrapper.appendChild(bannerItem);
-    });
-  
-    let currentIndex = 0;
-  
-    function updateBanner() {
-      const items = document.querySelectorAll('.banner-item');
-      const totalItems = items.length;
-      currentIndex = (currentIndex + 1) % totalItems;
-      const offset = -currentIndex * 100;
-      bannerWrapper.style.transform = `translateX(${offset}%)`;
-    }
-  
-    setInterval(updateBanner, 5000);
+  bannerWrapper.innerHTML = '';
+
+  iframes.forEach(iframe => {
+    const clone = iframe.cloneNode(true);
+    const bannerItem = document.createElement('div');
+    bannerItem.className = 'banner-item';
+    bannerItem.appendChild(clone);
+    bannerWrapper.appendChild(bannerItem);
+  });
+
+  let currentIndex = 0;
+
+  function updateBanner() {
+    const items = document.querySelectorAll('.banner-item');
+    const totalItems = items.length;
+    currentIndex = (currentIndex + 1) % totalItems;
+    const offset = -currentIndex * 100;
+    bannerWrapper.style.transform = `translateX(${offset}%)`;
+  }
+
+  setInterval(updateBanner, 5000);
 });
